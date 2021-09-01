12849f093c492b6a3dc53f4d176ebbd03c131e52
gjain0707 <87965825+gjain0707@users.noreply.github.com>
2021-09-01 10:21:59 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,14 @@
+Name = input ("Enter your Full name")
+print ("Your full name is " + Name)
+
+Age = input("Enter your present age")
+print ("Your present age is " + Age)
+
+Age = int(Age)
+
+calculate = 100 - Age
+a= 2020
+year = 2020 + calculate
+year = str(year)
+
+print (Name + " you will turn 100 years old in " + year )
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,10 @@
+Number = int(input("Please enter a number of your choice"))
+
+if (Number/2 ==0) :
+    Number = str(Number)
+    print ("The Number " + Number +" is even !")
+
+else:
+    Number = str(Number)
+    print ("The Number " + Number +" is odd !")
+    
